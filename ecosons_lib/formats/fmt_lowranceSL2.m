38309f444bd5fb33283c56c56c86a280fb8c4a86
daniel-rperez <26843566+daniel-rperez@users.noreply.github.com>
2024-04-25 16:43:26 +0200

    Add files via upload

--- ecosons_lib/formats/fmt_lowranceSL2.m
+++ ecosons_lib/formats/fmt_lowranceSL2.m
@@ -30,14 +30,14 @@ function [P,Q,G]=fmt_lowranceSL2(fname)
 	while( ~feof(f) )
 		%datagram header
 		hd=fread(f, 140, "uint8");
-		
+
 		nc=hd(29); %channel number
 		if( ~any( nc == nchannels ) )
 			nchannels=[nchannels, nc];
 			npings=[npings, 0];
 			lpings=[lpings, 0];
 		endif
-    
+
     %ping number (in channel?); keep maximum
     n=typecast(uint8(hd(33:36)'), "uint32")+1;
     npings(nchannels==nc)=max(n, npings(nc == nchannels));
@@ -56,23 +56,23 @@ function [P,Q,G]=fmt_lowranceSL2(fname)
       error('Error in file');
 			return
 		endif
-		
+
 		%count datagrams
 		ncount=ncount+1;
-	endwhile 
+	endwhile
 
   %pings and headers lists
 	P=[];
 	Q=struct;
   G=struct;
-	
+
   %GPS data
 	X=nan(1,ncount);
 	Y=nan(1,ncount);
 	T0=-1;
 	T=nan(1,ncount);
 	D=nan(1,ncount);
-	
+
 	for nc=nchannels(1)
 
 		%back to the beginning
@@ -81,7 +81,7 @@ function [P,Q,G]=fmt_lowranceSL2(fname)
 		%hd0
 		hd0=fread(f,8,'uint8');
 		ll=fread(f,1,'uint32');
-    
+
 		nping=0;
 		for n=1:ncount
 			%datagram header
@@ -94,7 +94,7 @@ function [P,Q,G]=fmt_lowranceSL2(fname)
 
  			if( nc == hd(29) )
         nping=nping+1;
-        
+
   			%GPS
         Gn=struct;
         xy=typecast(uint8(hd(105:112)'), "int32");
@@ -108,34 +108,34 @@ function [P,Q,G]=fmt_lowranceSL2(fname)
         Gn._day=dte.mday;
 
         G(nping)=Gn;
-        
+
         %Q
         Qn=struct;
           Qn.channel=nc;
           Qn.frequency=100e+3; %¿?
           Qn.transmitPower=1000; %¿?
           td=typecast(uint8(hd(41:44)'), "single");
-          Qn.sampleInterval=2*0.3048*td/(soundSpeed*l);
+          Qn.sampleInterval=0.3048*td/(soundSpeed*l);
           Qn.pulseLength=4*Qn.sampleInterval;
           Qn.soundVelocity=soundSpeed;
           Qn.absorptionCoefficient=0.0;
           Qn.temperature=temperature;
           Qn.count=n;
           dp=typecast(uint8(hd(61:64)'), "single"); %bad bottom detection in shallow areas
-          Qn._depth=0.3048*dp;        
-        
+          Qn._depth=0.5*0.3048*dp;
+
         Q(nping)=Qn;
 
         ec=fread(f,l,"uint8");
         if( isempty(P) )
-          P=nan(npings, l);
+          P=nan(npings(1), l);
         endif
         P(nping,1:l)=(ec'-255)/log10(2);
 
       else
         fseek(f, l, "cof");
       endif
-      
+
       ll=fread(f,1,'uint32');
       if( ll+4 ~= ftell(f) )
         error('Error in file')
@@ -147,5 +147,5 @@ function [P,Q,G]=fmt_lowranceSL2(fname)
 
   P={P};
   Q={Q};
-  
+
 endfunction
