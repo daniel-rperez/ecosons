d707d8859f57fe421795c5a0bbb03932d644d407
daniel-rperez <26843566+daniel-rperez@users.noreply.github.com>
2024-04-25 16:39:02 +0200

    Add files via upload

--- ecosons_lib/formats/fmt_simradRAW.m
+++ ecosons_lib/formats/fmt_simradRAW.m
@@ -30,11 +30,11 @@ function [P,HS,PS,At,Al,W,filt]=fmt_simradRAW(fname)
  %sizes
  max_channels=0;
  max_counts=[0];
- 
+
  %transceiver Z
  chTrcvZ=[0];
  filt=struct;
- 
+
  pass=0;
  while(pass < 2)
   pass=pass+1;
@@ -44,11 +44,11 @@ function [P,HS,PS,At,Al,W,filt]=fmt_simradRAW(fname)
   nPings=zeros(1,max_channels);
 
   while( ~feof(sonarFile) )
- 
+
    dgrm=readRawDatagram(sonarFile);
- 
+
    if( pass==1 )
-   
+
     if( strcmp(dgrm.type, 'XML0') && isfield(dgrm.xml, 'Configuration') )
      tcvs=dgrm.xml.Configuration.Transceivers.Transceiver;
      for nt=1:length(tcvs)
@@ -62,7 +62,7 @@ function [P,HS,PS,At,Al,W,filt]=fmt_simradRAW(fname)
       endfor
      endfor
     endif
-   
+
 %    if( strcmp(dgrm.type, 'XML0') && isfield(dgrm.xml, 'Channel') )
 %    %if( strcmp(dgrm.type, 'XML0') && strcmp(dgrm.xml.__name, 'Channels') )
 %     dgrm.xml
@@ -73,15 +73,15 @@ function [P,HS,PS,At,Al,W,filt]=fmt_simradRAW(fname)
 %      endif
 %     endfor
 %    endif
-  
+
     if( strcmp(dgrm.type, 'RAW0') || strcmp(dgrm.type, 'RAW3') )
-    
+
      %sample header
      lH=dgrm.sample;
      if( isfield(lH, 'channelId') )
        lH.channel=find( strcmp(lH.channelId, lChIds) );
      endif
-     
+
      ch=lH.channel;
 
      if( ch > max_channels )
@@ -105,13 +105,13 @@ function [P,HS,PS,At,Al,W,filt]=fmt_simradRAW(fname)
 
      nPings(ch)=nPings(ch)+1;
      max_counts(ch)=max(max_counts(ch),lH.count);
-        
+
     endif
-    
+
    else
-   
+
     switch(dgrm.type)
-   
+
     case 'NME0'
      llPS=gpsRead(dgrm.nmea);
      if( llPS.time >=0 )
@@ -171,14 +171,14 @@ function [P,HS,PS,At,Al,W,filt]=fmt_simradRAW(fname)
       lHS(ch).sampleInterval=str2num(chn._SampleInterval);
       lHS(ch).transmitPower=str2num(chn._TransmitPower);
      endif
-   
+
      if( isfield(dgrm.xml, 'Environment') ) %only one per file?
       for ch=1:length(lChIds)
        lHS(ch).transducerDepth=( depth=str2num(dgrm.xml.Environment._Depth) );
        lHS(ch).soundVelocity=str2num(dgrm.xml.Environment._SoundSpeed);
        lHS(ch).temperature=( temperature=str2num(dgrm.xml.Environment._Temperature) );
-       salinity=str2num(dgrm.xml.Environment._Salinity);
-       acidity=str2num(dgrm.xml.Environment._Acidity);
+       lHS(ch).salinity=( salinity=str2num(dgrm.xml.Environment._Salinity) );
+       lHS(ch).acidity=( acidity=str2num(dgrm.xml.Environment._Acidity) );
        %!!!disp(['f=' num2str(lHS(ch).frequency) ', T=' num2str(temperature) ', S=' num2str(salinity), ', D=' num2str(depth) ', pH=' num2str(acidity)])
        if( isfield(lHS(ch), 'frequency') )
         lHS(ch).absorptionCoefficient=alphaAinslieMcColm(lHS(ch).frequency,temperature,salinity,depth,acidity); %calculado a partir de la salinidad, el pH, la temperatura, ...
@@ -189,7 +189,7 @@ function [P,HS,PS,At,Al,W,filt]=fmt_simradRAW(fname)
      endif
 
     case 'MRU0'
-    
+
      for ch=1:length(lChIds)
       lHS(ch).heave=dgrm.heave;
       lHS(ch).roll=dgrm.roll;
@@ -198,7 +198,7 @@ function [P,HS,PS,At,Al,W,filt]=fmt_simradRAW(fname)
      endfor
 
     case 'CON0'
-    
+
      if( length(dgrm.transducer) > 0 )
       for ch=1:length(lChIds)
        lHS(ch).beamType=dgrm.transducer(1).beamType;
@@ -206,40 +206,40 @@ function [P,HS,PS,At,Al,W,filt]=fmt_simradRAW(fname)
        lHS(ch).equivalentBeamAngle=dgrm.transducer(1).equivalentBeamAngle;
       endfor
      endif
-     
+
     case 'RAW0'
-  
+
      %sample header
      lH=dgrm.sample;
      ch=lH.channel;
-     
+
      nPings(ch)=nPings(ch)+1;
-     
+
      %header and last GPS data
      HS{ch}(nPings(ch))=lH;
      PS(nPings(ch))=lPS;
-     
+
      %sample power
      if( isfield(dgrm.data, "power") )
-        
+
       ll=min(lH.count, length(dgrm.data.power));
       P{ch}(nPings(ch),1:ll)=dgrm.data.power(1:ll);
-     
+
      endif
-     
+
      %sample angles
      if( request_As(ch) ...
       && isfield(dgrm.data, "angleAthwartship") ...
       && isfield(dgrm.data, "angleAlongship") )
-  
+
       ll=min(lH.count, length(dgrm.data.power));
       At{ch}(nPings(ch),1:ll)=dgrm.data.angleAthwartship(1:ll);
       Al{ch}(nPings(ch),1:ll)=dgrm.data.angleAlongship(1:ll);
-     
+
      endif
 
     case 'RAW3'
-  
+
      %sample header
      lH=dgrm.sample;
      lH.channel=find( strcmp(lH.channelId, lChIds) );
@@ -249,41 +249,41 @@ function [P,HS,PS,At,Al,W,filt]=fmt_simradRAW(fname)
      for nf=1:length(ff)
       lH=setfield(lH, ff{nf}, getfield(lHS(ch),ff{nf}));
      endfor
-     
+
      nPings(ch)=nPings(ch)+1;
-     
+
      %header and last GPS data
      HS{ch}(nPings(ch))=lH;
      PS(nPings(ch))=lPS;
-     
+
      %sample power
      if( isfield(dgrm.data, "power") )
-        
+
       ll=min(lH.count, length(dgrm.data.power));
       P{ch}(nPings(ch),1:ll)=dgrm.data.power(1:ll);
-          
+
       %sample angles
       if( request_As(ch) ...
        && isfield(dgrm.data, "angleAthwartship") ...
        && isfield(dgrm.data, "angleAlongship") )
-  
+
        ll=min(lH.count, length(dgrm.data.power));
        At{ch}(nPings(ch),1:ll)=dgrm.data.angleAthwartship(1:ll);
        Al{ch}(nPings(ch),1:ll)=dgrm.data.angleAlongship(1:ll);
-     
+
       endif
-     
+
      endif
-      
+
      %waveform
      if( isfield(dgrm.data, "waveform") )
-      
+
       ll=min(lH.count, size(dgrm.data.waveform,1));
-      W{ch}(nPings(ch),1:ll,:)=dgrm.data.waveform(1:ll,:);
-      
+      W{ch}(nPings(ch),1:ll,:)=(1000 + 75)/1000 * dgrm.data.waveform(1:ll,:);
+
       dTX=round(2*lH.pulseLength/lH.sampleInterval); %!número de bins que contienen la señal transmitida
-      P{ch}(nPings(ch),1:ll-dTX)=20*log10( abs( mean(dgrm.data.waveform(1+dTX:ll,:),2) ) );
-            
+      P{ch}(nPings(ch),1:ll-dTX)=10*log10( 0.5*power(abs( mean(dgrm.data.waveform(1+dTX:ll,:),2) ), 2)/75 );
+
       if( request_As(ch) )
        if( isfield(lH, 'beamType') )
         bt=lH.beamType;
@@ -293,27 +293,27 @@ function [P,HS,PS,At,Al,W,filt]=fmt_simradRAW(fname)
        if( bt>0 )
         [paAt,paAl]=phaseAngle(squeeze(W{ch}(nPings(ch),1:ll,:)), bt);
         At{ch}(nPings(ch),1:ll-dTX)=paAt(1+dTX:end);
-        Al{ch}(nPings(ch),1:ll-dTX)=paAl(1+dTX:end);    
+        Al{ch}(nPings(ch),1:ll-dTX)=paAl(1+dTX:end);
        else
         At{ch}(nPings(ch),1:ll-dTX)=nan(1,ll-dTX);
         Al{ch}(nPings(ch),1:ll-dTX)=nan(1,ll-dTX);
        endif
       endif
      endif
-    
+
     case 'FIL1'
      ch=find( strcmp(dgrm.channelID, lChIds) );
      filt(ch).noOfCoefficients(dgrm.stage)=dgrm.noOfCoefficients;
      filt(ch).decimationFactor(dgrm.stage)=dgrm.decimationFactor;
-    
+
     endswitch
 
    endif
-    
+
   endwhile
 
   if( pass == 1 )
-   
+
    for ch=1:max_channels
     if( request_Wf(ch) )
      P{ch} = nan(nPings(ch),max_counts(ch));
@@ -326,12 +326,12 @@ function [P,HS,PS,At,Al,W,filt]=fmt_simradRAW(fname)
      At{ch}=nan(nPings(ch),max_counts(ch));
     endif
    endfor
-   
+
   endif
 
  endwhile %pass
  fclose(sonarFile);
- 
+
 endfunction
 
 
@@ -339,29 +339,34 @@ function lPS=gpsRead(nmea)
  lPS.time=-1.0;
  lPS.latitude=0.0;
  lPS.longitude=0.0;
- 
- if( length(nmea) > 5 )
- 
-  ss=strsplit(nmea(4:end), ',');
-  switch(ss{1})
-  
-  %https://gpsd.gitlab.io/gpsd/NMEA.html#_gga_global_positioning_system_fix_data
-  
-  case 'GGA' %GPS fix data: UTC lat lon
-   ll=ss{2};
-   lPS.time=str2num(ll(1:2))+str2num(ll(3:4))/60+str2num(ll(5:6))/3600;
 
-   ll=ss{3};
-   lPS.latitude=str2num(ll(1:2))+str2num(ll(3:end))/60;
-    if( ss{4} == 'S' )
-     lPS.latitude=-lPS.latitude;
-    endif
-   ll=ss{5};
-   lPS.longitude=str2num(ll(1:3))+str2num(ll(4:end))/60;
-    if( ss{6} == 'W' )
-     lPS.longitude=-lPS.longitude;
-    endif
-   
+ if( length(nmea) > 5 )
+  ss=strsplit(nmea(4:end), ',', 'collapsedelimiters', false);
+  switch(ss{1})
+
+  %https://gpsd.gitlab.io/gpsd/NMEA.html#_gga_global_positioning_system_fix_data
+
+  case 'GGA' %GPS fix data: UTC lat lon
+   %check for format errors (missing commas, non-numeric values) or missing coordinates
+   if( (length(ss) == 15) && ...
+       ~isempty(ss{2}) && ~isempty(ss{3}) && ~isempty(ss{5}) && ...
+       ~isalpha(ss{2}(1)) && ~isalpha(ss{3}(1)) && ~isalpha(ss{5}(1)) )
+
+    ll=ss{2};
+    lPS.time=str2num(ll(1:2))+str2num(ll(3:4))/60+str2num(ll(5:6))/3600;
+
+    ll=ss{3};
+    lPS.latitude=str2num(ll(1:2))+str2num(ll(3:end))/60;
+     if( ss{4} == 'S' )
+      lPS.latitude=-lPS.latitude;
+     endif
+    ll=ss{5};
+    lPS.longitude=str2num(ll(1:3))+str2num(ll(4:end))/60;
+     if( ss{6} == 'W' )
+      lPS.longitude=-lPS.longitude;
+     endif
+   endif
+
   case 'GLL' %Geographic position: latitude longitude UTC
    ll=ss{2};
    lPS.latitude=str2num(ll(1:2))+str2num(ll(3:end))/60;
@@ -378,7 +383,7 @@ function lPS=gpsRead(nmea)
      lPS.time=str2num(ll(1:2))+str2num(ll(3:4))/60+str2num(ll(5:6))/3600;
    else
      lPS.time=-1;
-   endif 
+   endif
 
   case 'GNS' %Fix data: UTC latitude longitude
    ll=ss{2};
@@ -409,9 +414,9 @@ function lPS=gpsRead(nmea)
     if( ss{6} == 'W' )
      lPS.longitude=-lPS.longitude;
     endif
-    
+
   endswitch
- 
+
  endif
 
 endfunction
@@ -439,7 +444,7 @@ function dgrm=readRawDatagram(sonarFile, ctx)
   hdr.transducerCount=fread(sonarFile, 1,'int32','ieee-le');
 
   dgrm.header=hdr;
-  
+
   dgrm.transducer=struct;
   for ntransducer=1:dgrm.header.transducerCount
    tr_des=struct;
@@ -469,24 +474,28 @@ function dgrm=readRawDatagram(sonarFile, ctx)
     fread(sonarFile, 8,'uchar');
    tr_des.gptSofwareVersion=sprintf('%c', fread(sonarFile,16,'uchar'));
     fread(sonarFile, 20,'uchar');
-   
+
 
    dgrm.transducer(ntransducer)=tr_des;
   endfor
-  
+
  case 'NME0'
   dgrm.nmea=sprintf('%c',fread(sonarFile,dgrm.length-3*4,'uchar'));
   %!!!disp(['NMEA=' dgrm.nmea]);
 
  case 'XML0'
   txt=sprintf('%c',fread(sonarFile,dgrm.length-3*4,'uchar'));
-  dgrm.xml=parseXML(txt);
+  try
+    dgrm.xml=parseXML(txt);
+  catch
+    disp(['XML: ' txt]);
+  end_try_catch
   %XML0: Transducers.Transducer(1:N), Transceivers.Transceiver(1:N), etc.
   %XML0: Channel(1:N).{Frequency,PulseDuration,SampleInterval,TransmitPower,SoundVelocity} también un único Channel
   %XML0: Environment.{Depth, SoundSpeed}
   %XML0: Sensor
   %XML0: Ping
-  
+
  case 'TAG0'
   dgrm.text=sprintf('%c',fread(sonarFile,dgrm.length-3*4,'uchar'));
    if( length(dgrm.text)>80 )
@@ -499,10 +508,10 @@ function dgrm=readRawDatagram(sonarFile, ctx)
   dgrm.roll=fread(sonarFile,1,'float32');
   dgrm.pitch=fread(sonarFile,1,'float32');
   dgrm.heading=fread(sonarFile,1,'float32');
- 
+
  case 'RAW0'
   samp=struct;
-  
+
   samp.channel=fread(sonarFile, 1,'int16','ieee-le');
   samp.mode=fread(sonarFile, 1,'int16','ieee-le');
   samp.transducerDepth=fread(sonarFile, 1,'float32','ieee-le');
@@ -520,7 +529,7 @@ function dgrm=readRawDatagram(sonarFile, ctx)
    fread(sonarFile, 12,'uchar');
   samp.offset=fread(sonarFile, 1,'int32','ieee-le');
   samp.count=fread(sonarFile, 1,'int32','ieee-le');
- 
+
   dat=struct;
   if( samp.mode==1 || samp.mode==3 )
    x=fread(sonarFile, samp.count,'int16','ieee-le');
@@ -542,7 +551,7 @@ function dgrm=readRawDatagram(sonarFile, ctx)
   samp.spare=fread(sonarFile, 1,'int16','ieee-le');
   samp.offset=fread(sonarFile, 1,'int32','ieee-le');
   samp.count=fread(sonarFile, 1,'int32','ieee-le');
- 
+
   dat=struct;
   if( bitand(samp.dataType,1) ) %Power: integer "compressed"
    x=fread(sonarFile, samp.count,'int16','ieee-le');
@@ -556,17 +565,17 @@ function dgrm=readRawDatagram(sonarFile, ctx)
   if( bitand(samp.dataType,4) ) %Waveform: complex float 16
    cperSample=floor(samp.dataType/256);
    x=fread(sonarFile, 2*cperSample*samp.count,'float16','ieee-le');
-   dat.waveform=reshape(x(1:2:end)+j*x(2:2:end), cperSample, samp.count)';
+   dat.waveform=reshape(x(1:2:end-1)+j*x(2:2:end), [cperSample, samp.count])';
   endif
   if( bitand(samp.dataType,8) ) %Waveform: complex float 32
    cperSample=floor(samp.dataType/256);
    x=fread(sonarFile, 2*cperSample*samp.count,'float32','ieee-le');
-   dat.waveform=reshape(x(1:2:end)+j*x(2:2:end), cperSample, samp.count)';
+   dat.waveform=reshape(x(1:2:end-1)+j*x(2:2:end), [cperSample, samp.count])';
   endif
 
   dgrm.sample=samp;
   dgrm.data=dat;
- 
+
 case 'FIL1'
   dgrm.stage=fread(sonarFile, 1,'int16','ieee-le');
   fread(sonarFile, 1,'char'); %!!!spec says 2 chars!
@@ -576,13 +585,13 @@ case 'FIL1'
   dgrm.noOfCoefficients=fread(sonarFile, 1,'int16','ieee-le');
   dgrm.decimationFactor=fread(sonarFile, 1,'int16','ieee-le');
   x=fread(sonarFile, 2*dgrm.noOfCoefficients,'float32','ieee-le');
-  dgrm.coefficients=x(1:2:end)+j*x(2:2:end);
+  dgrm.coefficients=x(1:2:end-1)+j*x(2:2:end);
 
  otherwise
   disp(['Unknown datagram: ' dgrm.type]);
   fread(sonarFile,dgrm.length-3*4,'uchar');
  endswitch
- 
+
  dgrm_lc=fread(sonarFile,1,'int32','ieee-le');
  if( dgrm_lc ~= dgrm.length )
   error('File does not conform Simrad Raw Format');
@@ -597,18 +606,18 @@ function alpha=alphaAinslieMcColm(f,T,S,D,pH) %Ainslie & McColm, J. Acoust. Soc.
  %T in ªC
  %S in PSU ~= 35
  %pH ~= 8
- 
+
  %Boric acid
  A1=0.106*exp((pH - 8)/0.56);
  P1=1;
  f1=0.78*sqrt(S/35)*exp(T/26);
- Boric=(A1*P1*f1*power(f,2))./(power(f,2)+f1**2);
+ Boric=(A1*P1*f1*power(f,2))./(power(f,2)+f1^2);
 
  %MgSO4 contribution
  A2=0.52*(S/35)*(1+T/43);
  P2=exp(-D/6);
  f2=42*exp(T/17);
- MgSO4=(A2*P2*f2*power(f,2))./(power(f,2)+f2**2);
+ MgSO4=(A2*P2*f2*power(f,2))./(power(f,2)+f2^2);
 
  %Pure water contribution
  A3=0.00049*exp(-(T/27+D/17));
---
+++ ecosons_lib/formats/parseXML.m
@@ -0,0 +1,211 @@
+% [xmlout, tagC, tagE]=parseXML(txt, xmlin)
+% Called as:
+%  xmlout = parseXML(txt)
+% Parses a text containing an xml document
+% txt: xml document or fragment (complete elements)
+% xmlin: the tag holding the document (or an empty document)
+%  used only internally for recursive calls
+% xmlout: octave struct holding the tag/document
+%  xmlout.__name: tag name
+%  xmlout.__prefix: tag prefix
+%  xmlout.__attribute: attribute
+%  xmlout.child: child xml tag (field name is tag name)
+%  xmlout.children(): same tag children list (field names are tag names)
+% tagC: boolean indicating whether the tag has been closed
+%  used only internally for recursive calls
+% tagE: position of txt where parsing has stopped (next character)
+%  used only internally for recursive calls
+function [xmlout, tagC, tagE]=parseXML(txt, xmlin, tagB)
+  
+  %straighten the text out (no newlines)
+  if( ~exist('xmlin') )
+    txt=strrep(strrep(txt, "\n", " "), "\r", "");
+
+    xmlout=struct;
+    xmlout.__name='.';
+  else
+    xmlout=xmlin;
+  endif
+    
+  tagC=false;
+  
+  if( exist('tagB') )
+    p=tagB;
+  else
+    p=1;
+  endif
+
+  while( p < length(txt) )
+  
+    %!
+    %disp( [num2str(p) ' / ' num2str(length(txt))] )
+    
+    stxt=txt(p:min([p+1024, length(txt)]));
+    try
+      regexp(stxt, '^.*$'); %!fails if stxt does not include complete utf-8 characters
+    catch
+      stxt=stxt(1:end-1);
+    end_try_catch
+    %closing tag: return
+    %[S,E]=regexp(txt(p:end), ['^</' xmlout.__name '\s*>'], 'once');
+    [S,E]=regexp(stxt, ['^</' xmlout.__name '\s*>'], 'once');
+    if( any(S) )
+      tagC=true;
+      p=p+E;
+      break;
+    endif
+
+    %spaces: ignore
+    %[S,E]=regexp(txt(p:end), '^\s+', 'once');
+    [S,E]=regexp(stxt, '^\s+', 'once');
+    if( any(S) )
+      p=p+E;
+      continue;
+    endif
+    
+    %initial XML declaration: ignore
+    %[S,E]=regexp(txt(p:end), '^<\?xml\s[^?]+\?>', 'once');
+    [S,E]=regexp(stxt, '^<\?xml\s[^?]+\?>', 'once');
+    if( any(S) )
+      p=p+E;
+      continue;
+    endif
+    
+    %tag with attributes
+    %[S,E]=regexp(txt(p:end), ['^<([a-zA-Z0-9:]+)\s*(([a-zA-Z0-9:]+)=("([^"]+)"|' "'" '([^' "'" ']+)' "'" ')\s*)+\s*/*>'], 'once');
+    [S,E]=regexp(stxt, ['^<([a-zA-Z0-9:]+)\s*(([a-zA-Z0-9:]+)=("([^"]+)"|' "'" '([^' "'" ']+)' "'" ')\s*)+\s*/*>'], 'once');
+    if( any(S) )
+
+      %[sxml,stagC,stagE]=parseXMLTag(txt(p:p+E-1));
+      [sxml,stagC,stagE]=parseXMLTag(stxt(1:E));
+      
+      p=p+E;
+      
+      %if the tag was not autoclosed read the contents
+      if( ~stagC )
+        [sxml, stagC, stagE]=parseXML(txt, sxml, p);
+        p=stagE;
+      endif
+      
+      %append field (if there was a previous one, add as a struct array)
+      if( isfield(xmlout, sxml.__name) )
+        f=getfield(xmlout, sxml.__name);
+        lf=length(f);
+        fn=fieldnames(sxml);
+        for nf=1:length(fn)
+          f=setfield(f, {lf+1}, fn{nf}, getfield(sxml,fn{nf}));
+        endfor
+        %f(end+1)=sxml;
+        xmlout=setfield(xmlout, sxml.__name, f);
+      else
+        xmlout=setfield(xmlout, sxml.__name, sxml);
+      endif
+      
+      continue
+    endif
+
+    %tag without attributes
+    %[S,E]=regexp(txt(p:end), ['^<([a-zA-Z0-9:]+)\s*/*>'], 'once');
+    [S,E]=regexp(stxt, ['^<([a-zA-Z0-9:]+)\s*/*>'], 'once');
+    if( any(S) )
+
+      %[sxml,stagC,stagE]=parseXMLTag(txt(p:p+E-1));
+      [sxml,stagC,stagE]=parseXMLTag(stxt(1:E));
+      
+      p=p+stagE;
+      
+      %if the tag was not autoclosed read the contents
+      if( ~stagC )
+        [sxml, stagC, stagE]=parseXML(txt, sxml, p);
+        p=stagE;
+      endif
+      
+      %append field (if there was a previous one, add as a struct array)
+      if( isfield(xmlout, sxml.__name) )
+        f=getfield(xmlout, sxml.__name);
+        lf=length(f);
+        fn=fieldnames(sxml);
+        for nf=1:length(fn)
+          f=setfield(f, {lf+1}, fn{nf}, getfield(sxml,fn{nf}));
+        endfor
+        %f(end+1)=sxml;
+        xmlout=setfield(xmlout, sxml.__name, f);
+      else
+        xmlout=setfield(xmlout, sxml.__name, sxml);
+      endif
+      
+      continue
+    endif
+    
+    %text in between: ignore?
+    [S,E]=regexp(txt(p:end), '^[^<]+', 'once');
+    if( any(S) )
+      %! do not ignore
+      if( isfield(xmlout, '__content') )
+        xmlout.__content=[xmlout.__content ' ' strtrim(txt(p:p+E-1))];
+      else
+        xmlout.__content=strtrim(txt(p:p+E-1));
+      endif
+      p=p+E;
+      continue
+    endif
+    
+    %spare <: ignore error
+    if( txt(p) == '<' )
+     p=p+1;
+    endif
+  
+  endwhile
+  
+  tagE=p;
+  
+endfunction
+
+%[xml, tagC, tagE]=parseXMLTag(txt)
+function [xml, tagC, tagE]=parseXMLTag(txt)
+  
+  xml=struct;
+  tagC=false;
+  
+  %tag name
+  [S,E, TE]=regexp(txt, '^<(([a-zA-Z0-9]+):)*([a-zA-Z0-9]+)\s*');
+  if( size(TE{1},1) > 1 )
+    xml.__prefix=txt(TE{1}(2,1):TE{1}(2,2));
+    xml.__name=txt(TE{1}(3,1):TE{1}(3,2));
+  else
+    xml.__name=txt(TE{1}(1,1):TE{1}(1,2));
+  endif
+
+
+  p=1+E;
+  while( p < length(txt) )
+
+    [S,E, TE]=regexp(txt(p:end), ['^([a-zA-Z0-9]+)\s*=\s*("([^"]+)"|' "'" '([^' "'" ']+)' "'" ')\s*']);
+    if( any(S) )
+      xml=setfield(xml, [ '_' txt(p-1+[TE{1}(1,1):TE{1}(1,2)]) ], txt(p-1+[TE{1}(3,1):TE{1}(3,2)]));
+      p=p+E;
+      continue
+    endif
+    
+    [S,E]=regexp(txt(p:end), '^/>');
+    if( any(S) )
+      tagC=true;
+      tagE=p+E;
+      return
+    endif
+    
+    [S,E]=regexp(txt(p:end), '^>');
+    if( any(S) )
+      tagC=false;
+      tagE=p+E;
+      return
+    endif
+    
+    %syntax error: skip one character
+    p=p+1;
+    
+  endwhile
+
+  tagE=p;
+  
+endfunction
